9a4dbfab654f06e74a844ad591f1e16dcdf64443
JMelvin <james@venice.fm>
2022-10-22 11:24:28 -0400

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas, useFrame } from "@react-three/fiber";
+import React, { Suspense, useRef } from "react";
+import Avatar from "./Avatar";
+import { getMousePos } from "./utils"
+import ReactDOM from "react-dom"
+import {ContactShadows, Environment, OrbitControls, Html} from "@react-three/drei";
 
-function App() {
+export default function App() {
+  const d = 8.25
+  const mouse = useRef({ x: 0, y: 0 })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+   <Canvas 
+      onMouseMove={e => (mouse.current = getMousePos(e))} camera={{ position: [0, 0, 4], fov: 50 }}
+     >
+        <Suspense fallback={null}>
+          <Avatar 
+          mouse={mouse}   />
+
+        </Suspense>
+        <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={false} enablePan={false} />
+      </Canvas>
+  </>
   );
 }
-
-export default App;
